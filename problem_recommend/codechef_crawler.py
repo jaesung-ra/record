080cfdff025b72d2b8deedeab8b7039e8f8dc498
jaesung-ra <84004349+jaesung-ra@users.noreply.github.com>
2024-06-19 21:10:11 +0900

    Add files via upload

--- problem_recommend/codechef_crawler.py
+++ problem_recommend/codechef_crawler.py
@@ -1,102 +1,227 @@
+import pymysql
 import requests
 from bs4 import BeautifulSoup
 import json
-import csv
-from collections import defaultdict
 from p_tqdm import p_map
-from tqdm.auto import tqdm
+from  tqdm.auto import tqdm
+import random
+import multiprocessing
 import time
-from datetime import datetime
-def crawler(username):
-    # dictionary that holds user,code, and submission data
-    csv_file=open('record.csv','a',newline="")
-    writer=csv.writer(csv_file)
-    code_dict=defaultdict(str)  
-    PAGE=0
-    user=username.strip()
-    url=f"https://www.codechef.com/recent/user?page={PAGE}&user_handle={user}"
-    header={
-            "authority": "www.codechef.com",
-            "path": f"/recent/user?page={PAGE}&user_handle={user}",
-            "referer": f"https://www.codechef.com/users/{user}"
-        }
-    payload={
-            "page":PAGE,
-            "user_handle":user
-        }
+from nordvpn_switcher import initialize_VPN,rotate_VPN,terminate_VPN
+from collections import defaultdict
+import os
+change_flag=False
+pf=open("prob_done.txt","r")
+done=pf.readlines()
+done=list(map(lambda s: s.strip(),done))
+pf.close()
+conn = pymysql.connect(host='127.0.0.1', user='root',
+                       password='0000', db='recodb', charset='utf8')
+cur = conn.cursor()
+cur.execute("select code from tmp group by code")
+datas = list(list(zip(*cur.fetchall()))[0])
+conn.close()
+
+
+for d in done:
+    if d in datas: datas.remove(d)
+ef=open("prob_error.txt","r")
+done=ef.readlines()
+done=list(map(lambda s: s.strip(),done))
+ef.close()
+for d in done:
+    if d in datas: datas.remove(d)
+
+def crawler(params):
+    code=params[0]
+    flag=params[1]
+    lock=params[2]
+    print(f"\nPID: {os.getpid()}, {code}     START")
+    rand=random.randint(0,201)
+    dict=defaultdict(int)
     with requests.session() as session:
-            
-            while True:
-                try:
-                    res=session.get(url,headers=header,data=payload)
-                    json_res=json.loads(res.text)
-                except:
-                    log_file=open("log.txt","a")
-                    date=datetime.today().strftime("%Y/%m/%d %H:%M:%S")  
-                    log_file.write(f"{date}: {user}, first exception\n")
-                    log_file.close()
-                    time.sleep(1800)
-                break
-            max_page=json_res["max_page"]
-            for i in range(max_page):
-                PAGE=i
-                url=f"https://www.codechef.com/recent/user?page={PAGE}&user_handle={user}"
-                header={
-                    "authority": "www.codechef.com",
-                    "path": f"/recent/user?page={PAGE}&user_handle={user}",
-                    "referer": f"https://www.codechef.com/users/{user}"
-                }
-                payload={
-                    "page":PAGE,
-                    "user_handle":user
-                }
+        res = session.get(f"https://www.codechef.com/status/{code}?page=0")
+        bs = BeautifulSoup(res.text, 'html.parser')
+        csrf = bs.findAll("script")[4].text[74:138]
+        res=session.post("https://www.codechef.com/api/codechef/login",data={"name":"jsr123","pass":"Dhwltla18~","csrfToken":csrf,"form_id":"ajax_login_form"})
+        url = f"https://www.codechef.com/api/submissions/PRACTICE/{code}?limit=20&page=0&usernames=&language=&status="
+        header = {
+            "authority": "www.codechef.com",
+            "path": f"https://www.codechef.com/api/submissions/PRACTICE/{code}?limit=20&page=0&usernames=&language=&status=",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+            "Referer": f"https://www.codechef.com/status/{code}?page=0",
+            "X-Csrf-Token": csrf
+        }
+        payload = {
+            "page": 0,
+            "limit": 20
+        }
+        res = session.get(url, headers=header, data=payload)
+        if res.status_code==403:
+            if flag.value==1:
                 while True:
-                    try:
-                        res=session.get(url,headers=header,data=payload)
-                        json_res=json.loads(res.text)
-                    except:
-                        log_file=open("log.txt","a")
-                        date=datetime.today().strftime("%Y/%m/%d %H:%M:%S") 
-                        log_file.write(f"{date}: {user}, {PAGE}, second exception\n")
-                        log_file.close()
-                        time.sleep(1800)
-                    break
-                soup=BeautifulSoup(json_res["content"],"html.parser")
-                tr=soup.find_all("a",class_=False)
-                for item in tr:
-                    code=item.get_text()
-                    if item.get("target") is None:
+                    print(f"\nPID: {os.getpid()}, SLEEPING..")
+                    time.sleep(60)
+                    if flag.value==0:
+                        time.sleep(60)
+                        print(f"\nPID: {os.getpid()}, WAKING..") 
                         break
-                    if code not in probs:
-                        continue
-                    img=item.parent.parent.select_one("img")["src"]
-                    if img=="https://cdn.codechef.com/misc/tick-icon.gif":
-                        code_dict[code]="1"+code_dict[code]
-                    else:
-                        code_dict[code]="0"+code_dict[code]
-            for code,score in code_dict.items():
-                data=[user,code,score]
-                writer.writerow(data)
-            user_done_file=open("user_done.txt","a")
-            user_done_file.write(f"{user}\n")
-            user_done_file.close()
-            csv_file.close()
-    del code_dict
-                
-# opening and loading data and storing them in global variables #
+            else:
+                with lock:
+                    flag.value=1
+                rotate_VPN()
+                with lock:
+                    flag.value=0
+            time.sleep(4)
+            res = session.get(f"https://www.codechef.com/status/{code}?page=0")
+            bs = BeautifulSoup(res.text, 'html.parser')
+            csrf = bs.findAll("script")[4].text[74:138]
+            res=session.post("https://www.codechef.com/api/codechef/login",data={"name":"jsr123","pass":"Dhwltla18~","csrfToken":csrf,"form_id":"ajax_login_form"})
+            header = {
+                "authority": "www.codechef.com",
+                "path": f"https://www.codechef.com/api/submissions/PRACTICE/{code}?limit=20&page=0&usernames=&language=&status=",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+                "Referer": f"https://www.codechef.com/status/{code}?page=0",
+                "X-Csrf-Token": csrf
+            }
+            res = session.get(url, headers=header, data=payload)
+        try:
+            json_res = json.loads(res.text)
+            max = int(json_res['count'])
+            max_page = max//100
+        except:
+            print(f"\nPID: {os.getpid()}, ERROR")
+            time.sleep(60)
+            res=session.get(url,headers=header,data=payload)
+            try:
+                json_res = json.loads(res.text)
+                max = int(json_res['count'])
+                max_page = max//100
+            except:
+                print(f"\nPID: {os.getpid()}, ERROR AGAIN")
+                return
+            '''
+            error_prob_file=open("prob_error.txt","a")
+            error_prob_file.write(f"{code}\n")
+            error_prob_file.close()
+            '''
+            return
+        if (max % 100 == 0):
+            max_page -= 1
+        for p in range(0, max_page+1):
+            url = f"https://www.codechef.com/api/submissions/PRACTICE/{code}?limit=100&page={p}&usernames=&language=&status="
+            header = {
+                "authority": "www.codechef.com",
+                "path": f"https://www.codechef.com/api/submissions/PRACTICE/{code}?limit=100&page={p}&usernames=&language=&status=",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+                "Referer": f"https://www.codechef.com/status/{code}?page={p}",
+                "X-Csrf-Token": csrf
+            }
+            payload = {
+                "page": p,
+                "limit": 100
+            }
+            time.sleep(rand/100)
+            res = session.get(url, headers=header, data=payload)
+            if res.status_code==403:
+                if flag.value==1:
+                    while True:
+                        print(f"\nPID: {os.getpid()}, SLEEPING..")
+                        time.sleep(60)
+                        if flag.value==0: 
+                            time.sleep(60)
+                            print(f"\nPID: {os.getpid()}, RESUMING..")
+                            break
+                else:
+                    with lock:
+                        flag.value=1
+                    rotate_VPN()
+                    with lock:
+                        flag.value=0
+                    print(f"\nPID: {os.getpid()}, VPN ROATATION FINISHED SLEEPING..")
+                    time.sleep(60)
+                res = session.get(f"https://www.codechef.com/status/{code}?page=0")
+                bs = BeautifulSoup(res.text, 'html.parser')
+                csrf = bs.findAll("script")[4].text[74:138]
+                res=session.post("https://www.codechef.com/api/codechef/login",data={"name":"jsr123","pass":"Dhwltla18~","csrfToken":csrf,"form_id":"ajax_login_form"})
+                header = {
+                    "authority": "www.codechef.com",
+                    "path": f"https://www.codechef.com/api/submissions/PRACTICE/{code}?limit=20&page=0&usernames=&language=&status=",
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+                    "Referer": f"https://www.codechef.com/status/{code}?page=0",
+                    "X-Csrf-Token": csrf
+                }
+                res = session.get(url, headers=header, data=payload)
+            try:
+                jres = json.loads(res.text)
+                list = jres['data']
+            except:
+                print(f"{code} error at page: {p}")
+                res=session.get(url,headers=header,data=payload)
+                time.sleep(10)
+                try:
+                    jres=json.laods(res.text)
+                    list=jres['data']
+                except:
+                    print(f"{code} at page: {p} error again")
+                    if(p>0):
+                        try:
+                            conn = pymysql.connect(host='127.0.0.1', user='root',
+                                        password='0000', db='recodb', charset='utf8')
+                            cur = conn.cursor()
+                            for key, value in dict.items():
+                                query = "insert into test (username,code,tooltip) values (%s,%s,%s)"
+                                if value == 1:
+                                    data = (key, code, 'r')
+                                else:
+                                    data = (key, code, 'w')
+                                cur.execute(query, data)
+                                conn.commit()
+                        finally:
+                            conn.close()
+                        del dict
+                        print(f"{code}     FINISH")
+                    error_prob_file=open("prob_done.txt","a")
+                    error_prob_file.write(f"{code}\n")
+                    error_prob_file.close()
+                    return
+            for d in list:
+                username = d['username']
+                tool = d['tooltip']
+                if tool == "accepted":
+                    dict[username] = 1
+                else:
+                    dict[username] = 0
+    try:
+        conn = pymysql.connect(host='127.0.0.1', user='root',
+                                            password='0000', db='recodb', charset='utf8')
+        cur = conn.cursor()
+        for key, value in dict.items():
+            query = "insert into test (username,code,tooltip) values (%s,%s,%s)"
+            if value == 1:
+                data = (key, code, 'r')
+            else:
+                data = (key, code, 'w')
+            cur.execute(query, data)
+            conn.commit()
+    finally:
+        conn.close()
+    del dict
+    done_prob_file=open("prob_done.txt","a")
+    done_prob_file.write(f"{code}\n")
+    done_prob_file.close()
+    print(f"\nPID: {os.getpid()}, {code}     FINISH")
 
-prob_file=open("probs.txt","r")
-probs=prob_file.readlines()
-probs = list(map(lambda s: s.strip(), probs))
-prob_file.close()
-user_file=open("left_users4.txt","r")
-users=user_file.readlines()
-user_number=len(users)
-user_file.close()
+if __name__ == "__main__":
+    initialize_VPN(save=1,area_input=['south korea, japan, germany,united states,united kingdom,canada,singapore,dallas,chicago,atlanta,miami,los angeles,new york,san francisco,seattle,buffalo,saint louis,denver,salt lake city,phoenix'])
+    manager=multiprocessing.Manager()
+    lock=manager.Lock()
+    flag=manager.Value(int,0)
+    pool=multiprocessing.Pool(processes=6)
+    pool.map(crawler,[(code,flag,lock)for code in done])
+    pool.close()
+    pool.join()
 
-if __name__=="__main__":
-    # using multiprocess with 8 cpus, and showing progress bar #
-    # only shows the progress bar of the total process #
-    # future adjustments: show the progress bar for each 8 multi-processes #
-    num_cpus=8
-    p_map(crawler,users,**{"num_cpus": num_cpus})
+
+
+
